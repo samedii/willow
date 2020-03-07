278c65fb92b378e2649f02c7dc3ec14c340eb754
Richard <samedii@gmail.com>
2020-03-07 22:46:57 +0100

    Cleanup

--- src/game/index.js
+++ src/game/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { GameEngine } from "react-game-engine";
-import Renderer from "./graphics/renderer";
 import Systems from "./systems";
 import Entities from "./entities";
-
-import ShaderPass from "./graphics/passes/shader-pass";
-import PixelShader from "./graphics/shaders/pixel-shader";
 
 import "../index.css";
 
@@ -16,9 +12,6 @@ class Game extends React.Component {
         className="game"
         systems={Systems}
         entities={Entities()}
-        renderer={Renderer(
-          new ShaderPass(PixelShader())
-        )}
       />
     );
   }
--- src/game/utils/three/fbx-loader.js
+++
@@ -1,6 +0,0 @@
-import Zlib from "three/examples/js/libs/inflate.min";
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
-
-global.Zlib = Zlib.Zlib;
-
-export default FBXLoader;
