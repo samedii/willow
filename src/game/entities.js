7e20680127858102516a14370b8ea54bc6cb618c
Richard <samedii@gmail.com>
2020-03-10 01:14:14 +0100

    Bunny is rendering

--- src/game/entities.js
+++ src/game/entities.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Box } from "./renderers";
-
+import { Box, Bunny } from "./renderers";
 
 export default async () => {
 
 	const entities = {
-		ericas_box: { x: 200,  y: 200, renderer: <Box /> },
-		pauls_box: { x: 50,  y: 50, renderer: <Box /> }
+		// ericas_box: { x: 200,  y: 200, renderer: <Box /> },
+		// pauls_box: { x: 50,  y: 50, renderer: <Box /> },
+		pauls_bunny: { x: 50,  y: 50, renderer: <Bunny /> },
 	}
 
 	return entities;
