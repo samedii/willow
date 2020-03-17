50c8670e37652013782f4a0e08bdcddea35a685c
Richard <samedii@gmail.com>
2020-03-17 22:06:59 +0100

    Test movement idea

--- src/game/entities.js
+++ src/game/entities.js
@@ -4,7 +4,10 @@ import { Box, Bunny } from "./components";
 export default async () => {
 
 	const entities = {
-		player_box: { x: 200,  y: 200, width: 100, height: 100, rotation: Math.PI / 4, renderer: <Box /> },
+		player_box: { 
+			x: 200,  y: 200, width: 20, height: 20, rotation: 0,
+			renderer: <Box />
+		},
 		// pauls_box: { x: 50,  y: 50, renderer: <Box /> },
 		pauls_bunny: { x: 100,  y: 200, renderer: <Bunny /> },
 	}
--- src/game/systems/move-box.js
+++ src/game/systems/move-box.js
@@ -1,4 +1,8 @@
 
+var
+  mouse_x = undefined,
+  mouse_y = undefined;
+
 
 const MoveBox = (entities, { input }) => {
   //-- I'm choosing to update the game state (entities) directly for the sake of brevity and simplicity.
@@ -6,13 +10,83 @@ const MoveBox = (entities, { input }) => {
   //-- Example: return { ...entities, t.id: { UPDATED COMPONENTS }};
   //-- That said, it's probably worth considering performance implications in either case.
 
-  const { payload } = input.find(x => x.name === "onMouseDown") || {};
+  // save last mouse position in order to aim abilities
+  const { payload } = input.find(x => x.name === "onMouseMove") || {};
 
   if (payload) {
-    const player_box = entities["player_box"];
+    mouse_x = payload.pageX;
+    mouse_y = payload.pageY;
+  }
 
-    player_box.x = payload.pageX;
-    player_box.y = payload.pageY;
+  const player_box = entities.player_box;
+
+  const roll_distance = 60;
+
+  if (player_box.animating) {
+    if (player_box.animation === 'roll_right') {
+      player_box.rotation += 0.1;
+      if (player_box.rotation >= player_box.target_rotation) {
+        player_box.rotation = player_box.target_rotation;
+        player_box.animating = false;
+        player_box.animation = '';
+      }
+
+      let angle = player_box.rotation;
+
+      player_box.x = player_box.pivot_x + roll_distance * Math.cos(angle);
+      player_box.y = player_box.pivot_y + roll_distance * Math.sin(angle);
+    }
+    if (player_box.animation === 'roll_left') {
+      player_box.rotation -= 0.1;
+      if (player_box.rotation <= player_box.target_rotation) {
+        player_box.rotation = player_box.target_rotation;
+        player_box.animating = false;
+        player_box.animation = '';
+      }
+
+      let angle = player_box.rotation;
+
+      player_box.x = player_box.pivot_x + roll_distance * Math.cos(angle);
+      player_box.y = player_box.pivot_y + roll_distance * Math.sin(angle);
+    }
+  }
+  else {
+
+    const { payload } = input.find(x => x.name === "onKeyPress") || {};
+
+    player_box.rotation = Math.atan2(
+      mouse_y - player_box.y,
+      mouse_x - player_box.x,
+    )
+
+    if (payload) {
+  
+      if (payload.key === 'a') {
+        player_box.animating = true;
+        player_box.animation = 'roll_left';
+        player_box.target_rotation = player_box.rotation - Math.PI / 2;
+        player_box.rotation = player_box.target_rotation + Math.PI
+        player_box.pivot_x = player_box.x + roll_distance * Math.cos(player_box.target_rotation);
+        player_box.pivot_y = player_box.y + roll_distance * Math.sin(player_box.target_rotation);
+      }
+      else if (payload.key === 'd') {
+        player_box.animating = true;
+        player_box.animation = 'roll_right';
+        player_box.target_rotation = player_box.rotation + Math.PI / 2;
+        player_box.rotation = player_box.target_rotation - Math.PI
+        player_box.pivot_x = player_box.x + roll_distance * Math.cos(player_box.target_rotation);
+        player_box.pivot_y = player_box.y + roll_distance * Math.sin(player_box.target_rotation);
+      }
+      else if (payload.key === 'w') {
+        player_box.y -= roll_distance;
+      }
+      else if (payload.key === 's') {
+        player_box.y += roll_distance;
+      }
+    }
+  
+
+
   }
 
   return entities;
