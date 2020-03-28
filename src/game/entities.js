ea0e84e2389645960fd41e295de6d5a295765f03
samedii <samedii@gmail.com>
2020-03-28 18:53:50 +0100

    WIP

--- src/game/entities.js
+++ src/game/entities.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Engine, World, Bodies } from "matter-js";
 import { Box, Bunny } from "./components";
+import { TilingSprite } from '@inlet/react-pixi';
+import Test from "../assets/background.png";
 
 
 export default async () => {
@@ -14,10 +16,17 @@ export default async () => {
 
 	const entities = {
 		physics: { engine: engine, world: world },
+		purple_background: {
+			width: 5000, height: 5000,
+			tilePosition: { x: 0, y: 0 },
+			tileScale: { x: 1, y: 1 },
+			image: Test,
+			renderer: <TilingSprite />,
+		},
+		pauls_bunny: { x: 100, y: 200, renderer: <Bunny /> },
 		player_box: {
 			renderer: <Box />, body: player_box_body,
 		},
-		pauls_bunny: { x: 100, y: 200, renderer: <Bunny /> },
 	}
 
 	return entities;
